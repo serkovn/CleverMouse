af9be924379c63e82a83c3a12d4d8551bdc2e2a6
CleverMouse <serkovn@mail.ru>
2020-11-12 09:58:37 +0300

    Добалено подсчет очков если ответ правильный и убирать жизни если неправильно

--- core/src/com/serkovn/clevermouse/CleverMouse.java
+++ core/src/com/serkovn/clevermouse/CleverMouse.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.*;
@@ -18,7 +19,7 @@ public class  CleverMouse extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture mouse;
 	Texture cheese1;
-	Texture cheese2;
+	Texture cross;
 	Texture cheese3;
 	Texture answer1;
 	Texture answer2;
@@ -36,8 +37,10 @@ public class  CleverMouse extends ApplicationAdapter {
 	Rectangle mRectangleMouse;
 	ShapeRenderer shapeRenderer;
 	Random mRandom;
-	boolean Truth = false;
+	boolean finish = false;
 	int amount;
+	int notAmount;
+	BitmapFont scoreFont;
 	
 	@Override
 	public void create () {
@@ -47,8 +50,7 @@ public class  CleverMouse extends ApplicationAdapter {
 		mRandom = new Random();
 		mouse = new Texture("mouse.png");
 		cheese1 = new Texture("cheese.png");
-		cheese2 = new Texture("cheese.png");
-		cheese3 = new Texture("cheese.png");
+		cross = new Texture("cross.png");
 		answer1 = new Texture("wood.png");
 		answer2 = new Texture("wood.png");
 		answer3 = new Texture("wood.png");
@@ -59,6 +61,9 @@ public class  CleverMouse extends ApplicationAdapter {
 		mRectangleMouse = new Rectangle();
 		answerTrue = new Rectangle();
 		tubeY = Gdx.graphics.getHeight()/2 + Gdx.graphics.getHeight()/4;
+		scoreFont = new BitmapFont();
+		scoreFont.setColor(Color.BLACK);
+		scoreFont.getData().setScale(8);
 	}
 
 	@Override
@@ -67,9 +72,23 @@ public class  CleverMouse extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(background, 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 		batch.draw(mouse, locationMouse, 0,Gdx.graphics.getWidth()/5,Gdx.graphics.getHeight()/6);
-		batch.draw(cheese1, 20, Gdx.graphics.getHeight() - 80,60,60);
-		batch.draw(cheese2, 100, Gdx.graphics.getHeight() - 80,60,60);
-		batch.draw(cheese2, 180, Gdx.graphics.getHeight() - 80,60,60);
+		if (notAmount == 0) {
+			batch.draw(cheese1, 20, Gdx.graphics.getHeight() - 100, 80, 80);
+			batch.draw(cheese1, 120, Gdx.graphics.getHeight() - 100, 80, 80);
+			batch.draw(cheese1, 220, Gdx.graphics.getHeight() - 100, 80, 80);
+		} else if (notAmount == 1) {
+			batch.draw(cheese1, 20, Gdx.graphics.getHeight() - 100, 80, 80);
+			batch.draw(cheese1, 120, Gdx.graphics.getHeight() - 100, 80, 80);
+			batch.draw(cross, 220, Gdx.graphics.getHeight() - 100, 80, 80);
+		} else if (notAmount == 2) {
+			batch.draw(cheese1, 20, Gdx.graphics.getHeight() - 100, 80, 80);
+			batch.draw(cross, 120, Gdx.graphics.getHeight() - 100, 80, 80);
+			batch.draw(cross, 220, Gdx.graphics.getHeight() - 100, 80, 80);
+		} else {
+			batch.draw(cross, 20, Gdx.graphics.getHeight() - 100, 80, 80);
+			batch.draw(cross, 120, Gdx.graphics.getHeight() - 100, 80, 80);
+			batch.draw(cross, 220, Gdx.graphics.getHeight() - 100, 80, 80);
+		}
 		mRectangleMouse = new Rectangle(locationMouse + 10, 0,Gdx.graphics.getWidth()/5 - 30,Gdx.graphics.getHeight()/6);
 
 		if (Gdx.input.justTouched()) {
@@ -87,7 +106,9 @@ public class  CleverMouse extends ApplicationAdapter {
 		if (tubeY < (Gdx.graphics.getHeight()/6) ) {
 			if (Math.abs(mRectangleMouse.getX()-answerTrue.getX()) <40) {
 				Gdx.app.log("intersector","Yes");
+				amount++;
 			} else {
+				notAmount++;
 				Gdx.app.log("intersector","Not");
 			}
 			tubeY += distanceBetweenAnswer;
@@ -114,7 +135,7 @@ public class  CleverMouse extends ApplicationAdapter {
 				answerTrue= new Rectangle( Gdx.graphics.getWidth() / 2 + Gdx.graphics.getWidth() / 4, (int) tubeY,answer4.getWidth(),answer4.getHeight());
 		}
 
-
+		scoreFont.draw(batch, String.valueOf(amount),Gdx.graphics.getWidth()-250, Gdx.graphics.getHeight()-20);
 		batch.end();
 
 		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
